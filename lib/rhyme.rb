36c11efb471ef1ee9a67f10eba6de620a4b3ae05
ikngtty <ikngtty@gmail.com>
2022-08-21 15:18:25 +0900

    separate `Vowel`

--- lib/rhyme.rb
+++ lib/rhyme.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'pronounce_array'
+require_relative 'vowel'
 
 MIN_RHYME_LENGTH = 5
 
@@ -8,7 +9,7 @@ class Rhyme
   class << self
     def detect(text)
       pronounces = PronounceArray.parse(text).to_a
-      vowels = get_vowels(pronounces)
+      vowels = Vowel.get_vowels(pronounces)
 
       rhyme_ranges = []
       (0..(pronounces.length - 2 * MIN_RHYME_LENGTH)).each do |i|
@@ -40,50 +41,6 @@ class Rhyme
     end
 
     private
-
-    def get_vowel(pronounce)
-      case pronounce
-      when 'ア', 'カ', 'ガ', 'サ', 'ザ', 'タ', 'ダ', 'ナ', 'ハ', 'バ', 'パ',
-        'マ', 'ヤ', 'ラ', 'ワ', 'キャ', 'ギャ', 'シャ', 'ジャ', 'チャ', 'ヂャ',
-        'ニャ', 'ヒャ', 'ビャ', 'ピャ', 'ミャ', 'リャ', 'ヴァ', 'ツァ', 'ヅァ',
-        'ファ', 'ァ'
-        'ア'
-      when 'イ', 'キ', 'ギ', 'シ', 'ジ', 'チ', 'ヂ', 'ニ', 'ヒ', 'ビ', 'ピ', 'ミ',
-        'リ', 'ウィ', 'ヴィ', 'ツィ', 'ヅィ', 'ティ', 'ディ', 'フィ', 'ィ'
-        'イ'
-      when 'ウ', 'ク', 'グ', 'ス', 'ズ', 'ツ', 'ヅ', 'ヌ', 'フ', 'ブ', 'プ',
-        'ム', 'ユ', 'ル', 'キュ', 'ギュ', 'シュ', 'ジュ', 'チュ', 'ヂュ',
-        'ニュ', 'ヒュ', 'ビュ', 'ピュ', 'ミュ', 'リュ', 'トゥ', 'ドゥ', 'ゥ'
-        'ウ'
-      when 'エ', 'ケ', 'ゲ', 'セ', 'ゼ', 'テ', 'デ', 'ネ', 'ヘ', 'ベ', 'ペ', 'メ',
-        'レ', 'イェ', 'ウェ', 'ヴェ', 'シェ', 'ジェ', 'チェ', 'ヂェ', 'ツェ',
-        'ヅェ', 'フェ', 'ェ'
-        'エ'
-      when 'オ', 'コ', 'ゴ', 'ソ', 'ゾ', 'ト', 'ド', 'ノ', 'ホ', 'ボ', 'ポ',
-        'モ', 'ヨ', 'ロ', 'ヲ', 'キョ', 'ギョ', 'ショ', 'ジョ', 'チョ', 'ヂョ',
-        'ニョ', 'ヒョ', 'ビョ', 'ピョ', 'ミョ', 'リョ', 'ウォ', 'ヴォ', 'ツォ',
-        'ヅォ', 'フォ', 'ォ'
-        'オ'
-      when 'ン'
-        'ン'
-      when 'ッ'
-        'ッ'
-      when 'ー'
-        'ー'
-      else
-        '*'
-      end
-    end
-
-    def get_vowels(pronounces)
-      prev = '*'
-      pronounces.map do |pronounce|
-        vowel = get_vowel(pronounce)
-        vowel = prev if vowel == 'ー'
-        prev = vowel
-        vowel
-      end
-    end
 
     def get_left_rhyme_vowels(vowels1, vowels2)
       ans = []
